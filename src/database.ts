e45470c987dc8a3eb2d9120418d3046874d013c1
thipereira02 <thiago.spereira02@gmail.com>
2022-01-15 11:30:14 -0300

    Setting server

---
+++ src/database.ts
@@ -0,0 +1,13 @@
+import './setup';
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const connection = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default connection;
